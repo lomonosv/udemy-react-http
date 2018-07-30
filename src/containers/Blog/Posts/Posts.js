1d05cacafc7c6858b7e0064716ef992ac81dc106
Vladimir Lomonos <vladimir.lomonos@dukascopy.com>
2018-07-30 10:33:23 +0300

    Loading full post

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import './Posts.css';
 import axios from '../../../axios';
@@ -38,11 +39,14 @@ class Posts extends Component {
 
     if (!this.state.error) {
       posts = this.state.posts.map(post => {
-        return <Post
-          key={ post.id }
-          title={ post.title }
-          author={ post.author }
-          clicked={ () => this.postSelectedHandler(post.id) }/>
+        return (
+          <Link key={ post.id } to={ '/' + post.id }>
+            <Post
+              title={ post.title }
+              author={ post.author }
+              clicked={ () => this.postSelectedHandler(post.id) }/>
+          </Link>
+        );
       });
     }
 
